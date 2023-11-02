43fde2c3c040fc9ddd440875463999030d8bd5c2
Ruhul <ruhul@appnap.io>
2023-11-02 17:47:58 +0600

    Add user card

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,28 @@
+import Image from "next/image";
+
 import { useGetUser } from "@/repo/api-hooks/useGetUser";
 
 export default function Home() {
   const { data: userData } = useGetUser(1, {});
 
   return (
-    <main>
-      <section>
+    <main className="w-screen border flex items-center justify-center">
+      <section className="space-y-3 w-full ">
         {userData?.data.map((user) => (
-          <div key={user.id}>
-            {user.id} {user.first_name} {user.last_name}
-          </div>
+          <article className="py-4 bg-slate-800 text-gray-400 rounded-md px-4 shadow-lg flex items-center space-x-4">
+            <p className="font-extrabold">{user.id}</p>
+            <Image
+              src={user.avatar}
+              width={50}
+              height={50}
+              className="rounded-full"
+              alt="user-profile"
+            />
+
+            <div key={user.id}>
+              {user.first_name} {user.last_name}
+            </div>
+          </article>
         ))}
       </section>
     </main>
