79a3867e06135ba4fe91d0fe719695b796a67177
Ruhul <ruhul@appnap.io>
2023-11-02 17:02:28 +0600

    add key

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,13 @@
 import { useGetUser } from "@/repo/api-hooks/useGetUser";
 
 export default function Home() {
-  const { data: userData, isLoading, isFetching } = useGetUser(1, {});
+  const { data: userData } = useGetUser(1, {});
 
   return (
     <main>
       <section>
         {userData?.data.map((user) => (
-          <div>
+          <div key={user.id}>
             {user.id} {user.first_name} {user.last_name}
           </div>
         ))}
